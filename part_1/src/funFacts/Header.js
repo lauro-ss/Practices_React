404175a70d82e5d40613b502c81b88324ccb4426
Lauro Santana Silva <bestlaurobr@gmail.com>
2023-01-10 22:14:58 -0300

    Add nav tag

--- part_1/src/funFacts/Header.js
+++ part_1/src/funFacts/Header.js
@@ -5,6 +5,11 @@ function Header() {
         <header>
             <div>
                 <img src={logo} alt="Logo do React" width="40px" />
+                <nav>
+                    <ul>Option 1</ul>
+                    <ul>Option 2</ul>
+                    <ul>Option 3</ul>
+                </nav>
             </div>
             <div>
                 <p>React 1</p>
