a600293d687cff4010acd4cbc8ce505c11d0f45b
Lauro Santana Silva <bestlaurobr@gmail.com>
2023-01-10 22:15:16 -0300

    Add Footer component

---
+++ part_1/src/funFacts/Footer.js
@@ -0,0 +1,9 @@
+function Footer(prop) {
+    return (
+        <footer>
+            <small>In Development. All rights reserved. 2023 - {prop.year}</small>
+        </footer>
+    );
+}
+
+export default Footer;
