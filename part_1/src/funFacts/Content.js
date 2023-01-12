cb0b58cce49a2abdfee3fb0c11175f6f1024aa92
Lauro Santana Silva <bestlaurobr@gmail.com>
2023-01-12 18:30:07 -0300

    Content style

--- part_1/src/funFacts/Content.js
+++ part_1/src/funFacts/Content.js
@@ -1,16 +1,17 @@
-function Content() {
+import '../funFactsCss/Content.css'
+
+
+export default function Content() {
     return (
-        <div>
+        <main>
             <h1>Fun facts about React</h1>
-            <ul>
+            <ul className='content-list'>
                 <li>Was first released in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
                 <li>Has well over 100k stars on Github</li>
                 <li>Is maintained by Facebook</li>
                 <li>Powers thousands of enterprise apps, including mobile apps</li>
             </ul>
-        </div>
+        </main>
     );
 }
-
-export default Content;
